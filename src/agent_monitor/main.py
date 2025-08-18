8af46922806a00ffcd725aaa150d2c88eee2001e
Felix Kampfer <felix.kampfer@qaware.de>
2025-08-18 13:11:59 +0200

    feat: PAAL-88 integrate frontend code with existing backend

--- src/agent_monitor/main.py
+++ src/agent_monitor/main.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from .api.routes.traces import router as trace_router
 from .api.routes.websockets import router as websocket_router
@@ -22,6 +23,9 @@ async def health_check():
 
 logger = logging.getLogger(__name__)
 
-# Include the routers
+# Include the API routers first (before static file mounting)
 app.include_router(trace_router)
 app.include_router(websocket_router)
+
+# Mount static files with SPA support
+app.mount("/", StaticFiles(directory="./frontend/dist/", html=True, check_dir=False), name="spa")
